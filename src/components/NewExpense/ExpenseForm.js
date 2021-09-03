7ca6ba4f2b8dfdaa5380c8b9d060f91844b6fd75
VWMAC <vincentkywong@gmail.com>
2021-09-02 23:39:41 -0700

    dateChangeHandler added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,7 @@ import './ExpenseForm.css';
 const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -12,7 +13,11 @@ const ExpenseForm = () => {
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-  }
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
 
   return (
     <form>
@@ -29,7 +34,7 @@ const ExpenseForm = () => {
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
 
