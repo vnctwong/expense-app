dac65eb14fcc00ca1e43cbc57d58f56676292eba
VWMAC <vincentkywong@gmail.com>
2021-09-02 23:32:23 -0700

    amountChangeHandler reaching state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,10 +4,15 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState(0);
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  }
 
   return (
     <form>
@@ -19,7 +24,7 @@ const ExpenseForm = () => {
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
 
         <div className="new-expense__control">
